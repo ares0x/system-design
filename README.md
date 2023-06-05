74b5c3d891540ce76c9e6fb7bebe425b080f241d
ares0x <jialijun1024@gmail.com>
2023-06-05 11:43:01 +0800

    delete useless file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # system-design
 
+crawler: 基于 goroutine 和 channel 的可控制并发的爬虫
+short-url: 短 url  
+
---
+++ short-url/internal/service/url_test.go
@@ -0,0 +1,11 @@
+package service
+
+import "testing"
+
+func TestUrlService_Decode(t *testing.T) {
+
+}
+
+func TestUrlService_Encode(t *testing.T) {
+
+}
